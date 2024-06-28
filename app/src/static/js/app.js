b574cce6408260836b6f60f27140bae119505570
ramya <ramyashree.21ai035@sode-edu.in>
2024-06-28 20:41:42 +0530

    Todo message

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no todo items yet! Add your todo list!</p>
+                <p className="text-center">You have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
