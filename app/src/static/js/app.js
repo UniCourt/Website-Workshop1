e03b01f3e27eb2f9ff42d93c4528cf63f047e4a3
BeastxD7 <shashank.21ai047@sode-edu.in>
2024-06-30 21:22:28 +0530

    updated todo message

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center custom">You have no todo items yet! Add one to see the items!</p>
+                <p className="text-center custom">You have no todo items yet! Add one Now!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
